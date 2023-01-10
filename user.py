662ba9683ae8e097ad6de4991a547a5f1962447d
Ruveyda <albayrakruveyda@gmail.com>
2023-01-10 14:29:02 -0800

    user.py update

--- user.py
+++ user.py
@@ -9,35 +9,41 @@ class User:
 
     def display_info(self):
         print (self.first_name,self.last_name,self.email,self.age,sep='\n')
+        return self
 
     def enroll(self):
         if self.is_rewards_member == True:
             print('User already a member.')
-            return False
+        
         else:
             self.is_rewards_member = True 
             self.gold_card_points = 200
-            return True
+        return self        
 
 
     def spend_points(self,amount):
         if self.gold_card_points >= amount:
             self.gold_card_points -= amount
-            return self.gold_card_points
+            print (self.gold_card_points)
         else:
             print ('User doesnot have enough points.')
+        return self
+
+
+
 
 id1 = User('Jane','Doe','Janedoe@email.com','32')
 id1.display_info()
+id1.enroll().spend_points(100)
 
-id2 = User('Jack','Brown','Jackbrown@email.com','45')
-id3 = User('Mary','Thomson','Marythomson@email.com','61') 
-id1.enroll()
-print (id1.spend_points(50))  
-id2.enroll()
-print (id2.spend_points(80))
+# id2 = User('Jack','Brown','Jackbrown@email.com','45')
+# id3 = User('Mary','Thomson','Marythomson@email.com','61') 
+# id1.enroll()
+# print (id1.spend_points(50))  
+# id2.enroll()
+# print (id2.spend_points(80))
 
-id2.display_info()
-id3.display_info()
-id1.enroll()
-id1.spend_points(250)
+# id2.display_info()
+# id3.display_info()
+# id1.enroll()
+# id1.spend_points(250)
