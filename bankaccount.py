e3639f40a87b0a66e0efb71b6625a8198d0bb103
Ruveyda <albayrakruveyda@gmail.com>
2023-01-10 15:40:03 -0800

    first commit

---
+++ bankaccount.py
@@ -0,0 +1,38 @@
+class BankAccount:
+    int_rate = 0.05
+    balance = 0
+
+    def __init__ (self, int_rate, balance):
+        self.int_rate = int_rate
+        self.balance = balance
+
+    def deposit(self,amount):
+        self.balance += amount
+        return self 
+
+    def withdraw (self,amount):
+        if self.balance >= amount:
+            self.balance -= amount
+        else:
+            self.balance -= 5
+            print ("insufficient funds:Charging a $5 fee")
+        return self    
+
+    def display_account_info(self):
+        print (f"Balance:${self.balance}")
+        return self
+
+    def yield_interest (self):
+        if self.balance > 0:
+            self.balance = self.balance + self.balance * self.int_rate
+        else:
+            print("Insufficient funds")
+        return self
+account1 = BankAccount(0.1,500)
+account2 = BankAccount(0.5,200)
+
+account1.deposit(50).deposit(25).deposit(25).withdraw(200).yield_interest().display_account_info()
+account2.deposit(100).deposit(50).withdraw(100).withdraw(150).withdraw(100).withdraw(1000).yield_interest().display_account_info()
+
+        
+
